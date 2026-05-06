94074959cecbf7e11c5154d523c66c24c8d9ce2c
bilol <bilolyoqubjonov2@gmail.com>
2026-05-06 19:31:30 +0500

    Update branches response logic

--- src/features/organization/branches/Admin/command/update-branches/update-branches.response.ts
+++ src/features/organization/branches/Admin/command/update-branches/update-branches.response.ts
@@ -1,3 +1,32 @@
-export class UpdateBranchesResponse {
+import {Expose} from "class-transformer";
+import {ApiProperty} from "@nestjs/swagger";
 
+export class UpdateBranchesResponse {
+  @Expose()
+  @ApiProperty()
+  id! : number
+
+  @Expose()
+  @ApiProperty()
+  countryId: number;
+
+  @Expose()
+  @ApiProperty()
+  representativeId: number;
+
+  @Expose()
+  @ApiProperty()
+  city: string;
+
+  @Expose()
+  @ApiProperty()
+  latitude: number;
+
+  @Expose()
+  @ApiProperty()
+  longitude: number;
+
+  @Expose()
+  @ApiProperty()
+  phoneNumber: string;
 }
